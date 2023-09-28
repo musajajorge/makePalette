aafeeccdae77caa11998a9adfc1ae4bcfa13845a
Jorge L. C. Musaja <73267844+musajajorge@users.noreply.github.com>
2023-09-28 17:36:46 -0500

    Delete R directory

--- R/msg.R
+++
@@ -1,9 +0,0 @@
-.onAttach <-
-  function(libname, pkgname)
-  {
-    #packageStartupMessage("")
-
-    packageStartupMessage("For examples, please visit https://github.com/musajajorge/makePalette")
-
-    packageStartupMessage("")
-  }
