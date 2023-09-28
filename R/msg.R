7c88db918184e9861591c1caa3b389aa1a7df926
Jorge L. C. Musaja <73267844+musajajorge@users.noreply.github.com>
2023-09-28 17:57:41 -0500

    version 0.1.2

---
+++ R/msg.R
@@ -0,0 +1,9 @@
+.onAttach <-
+  function(libname, pkgname)
+  {
+    #packageStartupMessage("")
+
+    packageStartupMessage("For examples, please visit https://github.com/musajajorge/makePalette")
+
+    packageStartupMessage("")
+  }
